0c7bfdb0d2bfa027ae04751f453dddf485e82173
Lubomir Rajter <lubomir.rajter@gmail.com>
2023-01-09 10:28:48 +0100

    pr2_to_stampa script added

---
+++ scripts2/pr2_to_stampa.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# This shell script downloads and trims the PR2 database for the stampa pipeline
+# adjusted from Frederic Mahe's script: https://github.com/frederic-mahe/stampa
+
+
+# 1. download the UTAX version of the PR2 database
+VERSION="4.14.0"
+URL="https://github.com/pr2database/pr2database/releases/download"
+SOURCE="pr2_version_${VERSION}_SSU_UTAX.fasta"
+LOCATION="../raw_data/stampa"
+
+wget -P ${LOCATION} "${URL}/v${VERSION}/${SOURCE}.gz"
+
+gunzip -k ${LOCATION}/${SOURCE}.gz
+
+# 2. extract the V4 region from the downloaded PR2 database
+
+PRIMER_F="CCAGCASCYGCGGTAATTCC"
+PRIMER_R="TYRATCAAGAACGAAAGT"
+OUTPUT="${SOURCE/_UTAX*/}_${PRIMER_F}_${PRIMER_R}.fas"
+LOG="${OUTPUT/.fas/.log}"
+MIN_LENGTH=32
+MIN_F=$(( ${#PRIMER_F} * 2 / 3 ))
+MIN_R=$(( ${#PRIMER_R} * 2 / 3 ))
+CUTADAPT="$(which cutadapt) --discard-untrimmed --minimum-length ${MIN_LENGTH}"
+
+dos2unix < "${LOCATION}/${SOURCE}" | \
+    sed '/^>/ s/;tax=k:/ /
+         /^>/ s/,[dpcofgs]:/|/g
+         /^>/ ! s/U/T/g' | \
+    ${CUTADAPT} -g "${PRIMER_F}" -O "${MIN_F}" - 2> "${LOCATION}/${LOG}" | \
+    ${CUTADAPT} -a "${PRIMER_R}" -O "${MIN_R}" - 2>> "${LOCATION}/${LOG}" > "${LOCATION}/${OUTPUT}"
