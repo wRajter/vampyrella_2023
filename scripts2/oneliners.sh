cb93fc7ea376e3e2af1d241589918691ff4a8f0e
Lubomir Rajter <lubomir.rajter@gmail.com>
2023-01-21 14:39:07 +0100

    all scripts updated

--- scripts2/oneliners.sh
+++ scripts2/oneliners.sh
@@ -7,3 +7,6 @@ sed 's/-//g' vamp_refseq_2022.fasta | grep "\S" > vamp_refseq_2022_unalign.fasta
 
 # insert taxpath to the ref alignment
 sed 's/>*_/&;tax=k:Eukaryota,d:Rhizaria,p:Cercozoa,c:Endomyxa,o:Vampyrellida,f:,g:,s:  /' review_vamp_ref_2022.fasta
+
+# Extract fasta sequences from a file using a list in another file
+seqtk subseq test.fa test.txt
---
+++ scripts2/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SAMPLES="ALL_SAMPLES \
+         A3_ \
+         MOCK"
+
+for SAMPLE in $SAMPLES
+do
+  echo "sample: ${SAMPLE}"
+done
