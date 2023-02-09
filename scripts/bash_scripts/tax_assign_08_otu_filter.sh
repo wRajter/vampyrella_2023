4f9ea939f10e76b0e2b3bedfda67b306af6ee58a
Lubomir Rajter <lubomir.rajter@gmail.com>
2023-02-09 11:08:00 +0100

    filter otu after tax. assignment script added

--- scripts/bash_scripts/tax_assign_08_otu_filter.sh
+++ scripts/bash_scripts/tax_assign_08_otu_filter.sh
@@ -6,67 +6,68 @@
 # Variables:
 CELL="cellCombined"
 MARKER="Full18S"
+SIM="sim97"
 RAW_DATA="../../raw_data"
-DADA2_OUT="${RAW_DATA}/qiime_output/dada2_output"
-TAX_ASSIGN_DIR="${RAW_DATA}/qiime_output/assignment_results/${MARKER}_${CELL}"
-FIL_ASSIGN_OUT="${RAW_DATA}/filt_assign_output/${MARKER}/${CELL}"
-MIN_FREQ_THRESHOLD=13
-MAX_DEPTH=31807
-SEQ_DEPTH_CUTOFF=50
+OTU_CLUST_DIR="${RAW_DATA}/OTU_clust/${MARKER}/${CELL}/${SIM}"
+TAX_ASSIGN_DIR="${RAW_DATA}/tax_assign_results/${MARKER}/${CELL}/${SIM}"
+FILT_OTU_DIR="${RAW_DATA}/OTU_filtered/${MARKER}/${CELL}/${SIM}"
+
+# DADA2_OUT="${RAW_DATA}/qiime_output/dada2_output"
+# TAX_ASSIGN_DIR="${RAW_DATA}/qiime_output/assignment_results/${MARKER}_${CELL}"
+# FIL_ASSIGN_OUT="${RAW_DATA}/filt_assign_output/${MARKER}/${CELL}"
+
+# MIN_FREQ_THRESHOLD=13
+# MAX_DEPTH=31807
+# SEQ_DEPTH_CUTOFF=50
 
 
 ##########################
-## FILTER OUT RARE ASVs ##
+## FILTER OUT RARE OTUs ##
 ##########################
 
-echo "Working on ${MARKER} marker and ${CELL} cell."
+echo "Working on ${MARKER} ${CELL} ${SIM} sample."
 
 # Filtering the denoised table to remove noise/singletons
-echo "Filtering out rare ASVs..."
+echo "Filtering out rare OTUs..."
 
 # Cleaning
-mkdir -p ${FIL_ASSIGN_OUT}
-rm -f ${FIL_ASSIGN_OUT}/table_filt_${MARKER}_${CELL}.qza
+mkdir -p ${FILT_OTU_DIR}/
+rm -f ${FILT_OTU_DIR}/table_filt.qza
 
-# Filtering out rare ASVs
-# Note:
-# To calculate the cut-off for excluding rare ASVs,
-# I use the mean sample depth.
-# This number could be find in the dada2 summary table,
-# which was created running the reads2asv script.
-# Then, I multiply the mean sample depth by 0.001, and round to the nearest integer.
-# This represents the min frequency threshold.
+# Filtering out rare OTUs
+
+read -p "Enter minimal OTU frequency threshold (more details in help.txt): " MIN_FREQ_THRESHOLD
 
 qiime feature-table filter-features \
-   --i-table ${DADA2_OUT}/table_${MARKER}_${CELL}.qza \
+   --i-table ${OTU_CLUST_DIR}/otu_table.qza \
    --p-min-frequency ${MIN_FREQ_THRESHOLD} \
    --p-min-samples 1 \
-   --o-filtered-table ${FIL_ASSIGN_OUT}/table_filt_${MARKER}_${CELL}.qza
+   --o-filtered-table ${FILT_OTU_DIR}/table_filt.qza
 
 
 ##################################################
-## FILTER OUT CONTAMINANT AND UNCLASSIFIED ASVs ##
+## FILTER OUT CONTAMINANT AND UNCLASSIFIED OTUs ##
 ##################################################
 
-# Removing ASVs which are likely contaminants or noise based on the taxonomic labels
-echo "Filtering out contaminant and unclassified ASVs in the ${MARKER} ${CELL} sample..."
+# Removing OTUs which are likely contaminants or noise based on the taxonomic labels
+echo "Filtering out contaminant and unclassified OTUs..."
 
 # Cleaning
-rm -f ${FIL_ASSIGN_OUT}/table_filt_contam_${MARKER}_${CELL}.qza
+rm -f ${FILT_OTU_DIR}/table_filt_contam.qza
 
 # Filtering
 # Note:
-# In this step, we will remove any ASV,
+# In this step, we will remove any OTU,
 # which contains word mitochondrial and chloroplast in its taxonomic label.
-# Then, we will exclude any ASV that is unclassified at the phylum level
+# Then, we will exclude any OTU that is unclassified at the phylum level
 # as those sequences could be noise (e.g. possible chimeric sequences).
 
 qiime taxa filter-table \
-   --i-table ${FIL_ASSIGN_OUT}/table_filt_${MARKER}_${CELL}.qza \
-   --i-taxonomy ${TAX_ASSIGN_DIR}/vsearch_taxonomy_${MARKER}_${CELL}_ALLSAMPLES.qza \
+   --i-table ${FILT_OTU_DIR}/table_filt.qza \
+   --i-taxonomy ${TAX_ASSIGN_DIR}/vsearch_taxonomy.qza \
    --p-include p__ \
    --p-exclude mitochondria,chloroplast \
-   --o-filtered-table ${FIL_ASSIGN_OUT}/table_filt_contam_${MARKER}_${CELL}.qza
+   --o-filtered-table ${FILT_OTU_DIR}/table_filt_contam.qza
 
 
 ########################
@@ -76,50 +77,54 @@ qiime taxa filter-table \
 # Visualizing rarefaction curves to determine the read depth plateaus and
 # to see the richness of the samples
 
-# Removing ASVs which are likely contaminants or noise based on the taxonomic labels
-echo "Creating rarefaction curves for the ${MARKER} ${CELL} sample..."
+# Removing OTUs which are likely contaminants or noise based on the taxonomic labels
+echo "Creating rarefaction curves..."
 
 # Cleaning
-rm -f ${FIL_ASSIGN_OUT}/table_filt_contam_summary_${MARKER}_${CELL}.qzv \
-      ${FIL_ASSIGN_OUT}/rarefaction_curves_test_${MARKER}_${CELL}.qzv \
-      ${FIL_ASSIGN_OUT}/table_final_${MARKER}_${CELL}.qza
+rm -f ${FILT_OTU_DIR}/table_filt_contam_summary.qzv \
+      ${FILT_OTU_DIR}/rarefaction_curves.qzv \
+      ${FILT_OTU_DIR}/otu_table_filtered.qza
 
-# Before creating rarefaction curves, we need to summarize the filtered table.
-# From this summary table, we can determine the maximum depth across your samples:
-# the maximum sample total frequency.
-# This number will be used as a threshold in the next step (--p-max-depth parameter).
+# Before creating rarefaction curves, we need to summarize the filtered table
+# to determine the maximum depth across the samples.
 qiime feature-table summarize \
-   --i-table ${FIL_ASSIGN_OUT}/table_filt_contam_${MARKER}_${CELL}.qza \
-   --o-visualization ${FIL_ASSIGN_OUT}/table_filt_contam_summary_${MARKER}_${CELL}.qzv
+   --i-table ${FILT_OTU_DIR}/table_filt_contam.qza \
+   --o-visualization ${FILT_OTU_DIR}/table_filt_contam_summary.qzv
+
+read -p "Enter maximum sequencing depth (more details in help.txt): " MAX_DEPTH
 
 # Creating rarefaction curves
 qiime diversity alpha-rarefaction \
-   --i-table ${FIL_ASSIGN_OUT}/table_filt_contam_${MARKER}_${CELL}.qza \
+   --i-table ${FILT_OTU_DIR}/table_filt_contam.qza \
    --p-max-depth ${MAX_DEPTH} \
    --p-steps 20 \
    --p-metrics 'observed_features' \
-   --o-visualization ${FIL_ASSIGN_OUT}/rarefaction_curves_test_${MARKER}_${CELL}.qzv
+   --o-visualization ${FILT_OTU_DIR}/rarefaction_curves.qzv
 
 
 # We can now examine the rarefication curves.
 # If we want to exclude some sample(s) with poor sequncing depth,
 # we can decide on a minimum depth cut-off.
-# Once we decide on a hard cut-off we can exclude samples below
-# this cut-off using the command below (where SEQ_DEPTH_CUTOFF is a placeholder
-# for the minimum depth you select):
-
-# qiime feature-table filter-samples \
-#    --i-table ${FIL_ASSIGN_OUT}/table_filt_contam_${MARKER}_${CELL}.qza \
-#    --p-min-frequency SEQ_DEPTH_CUTOFF \
-#    --o-filtered-table ${FIL_ASSIGN_OUT}/table_final_${MARKER}_${CELL}.qza
-
-# Alternatively, if we want to retain all samples then
+# Otherwise we will retain all samples then
 # we can simply make a copy of the QZA file with
-# the final table filename
+# the final table filename (see loop below).
 
-cp ${FIL_ASSIGN_OUT}/table_filt_contam_${MARKER}_${CELL}.qza \
-   ${FIL_ASSIGN_OUT}/table_final_${MARKER}_${CELL}.qza
+read -p "Do you want to exclude sample(s) based on sequencing depth (more details in help.txt) [Yes/No]: " FILT_SAMPLES
 
+if [ "${FILT_SAMPLES}" = "Yes" ]
+then
+  read -p "Enter sequence depth cut-off (more details in help.txt): " SEQ_DEPTH_CUTOFF
+  qiime feature-table filter-samples \
+    --i-table ${FILT_OTU_DIR}/table_filt_contam.qza \
+    --p-min-frequency ${SEQ_DEPTH_CUTOFF} \
+    --o-filtered-table ${FILT_OTU_DIR}/otu_table_filtered.qza
+elif [ "${FILT_SAMPLES}" = "No" ]
+then
+  cp ${FILT_OTU_DIR}/table_filt_contam.qza \
+    ${FILT_OTU_DIR}/otu_table_filtered.qza
+else
+  echo "Please answer Yes/No"
+fi
 
 
 #########################################
@@ -127,21 +132,35 @@ cp ${FIL_ASSIGN_OUT}/table_filt_contam_${MARKER}_${CELL}.qza \
 #########################################
 
 # Now that we have our final filtered table,
-# we will need to subset the QZA of ASV sequences to the same set.
+# we will need to subset the QZA of OTU sequences to the same set.
 
-echo "Subsetting the ASV sequences based the filtered table for ${MARKER} ${CELL} sample..."
+echo "Subsetting the OTU sequences based the filtered table..."
 
 # Cleaning
-rm -f ${FIL_ASSIGN_OUT}/asv_seqs_final_${MARKER}_${CELL}.qza \
-      ${FIL_ASSIGN_OUT}/table_final_summary_${MARKER}_${CELL}.qzv
+rm -f ${FILT_OTU_DIR}/otu_seqs_filtered.qza \
+      ${FILT_OTU_DIR}/otu_seqs_filtered.qzv \
+      ${FILT_OTU_DIR}/otu_seqs_filtered.fasta \
+      ${FILT_OTU_DIR}/table_filtered_summary.qzv
+
 
 # Subsetting
 qiime feature-table filter-seqs \
-   --i-data ${DADA2_OUT}/asv_${MARKER}_${CELL}_ALLSAMPLES.qza \
-   --i-table ${FIL_ASSIGN_OUT}/table_final_${MARKER}_${CELL}.qza \
-   --o-filtered-data ${FIL_ASSIGN_OUT}/asv_seqs_final_${MARKER}_${CELL}.qza
+   --i-data ${OTU_CLUST_DIR}/otu_seqs.qza \
+   --i-table ${FILT_OTU_DIR}/otu_table_filtered.qza \
+   --o-filtered-data ${FILT_OTU_DIR}/otu_seqs_filtered.qza
 
-# Creating final summary table for filtered ASVs
+qiime feature-table tabulate-seqs \
+    --i-data ${FILT_OTU_DIR}/otu_seqs_filtered.qza \
+    --o-visualization ${FILT_OTU_DIR}/otu_seqs_filtered.qzv
+
+qiime tools export \
+  --input-path ${FILT_OTU_DIR}/otu_seqs_filtered.qza \
+  --output-path ${FILT_OTU_DIR}/
+
+mv ${FILT_OTU_DIR}/dna-sequences.fasta \
+   ${FILT_OTU_DIR}/otu_seqs_filtered.fasta
+
+# Creating final summary table for filtered OTUs
 qiime feature-table summarize \
-   --i-table ${FIL_ASSIGN_OUT}/table_final_${MARKER}_${CELL}.qza  \
-   --o-visualization ${FIL_ASSIGN_OUT}/table_final_summary_${MARKER}_${CELL}.qzv
+   --i-table ${FILT_OTU_DIR}/otu_table_filtered.qza  \
+   --o-visualization ${FILT_OTU_DIR}/table_filtered_summary.qzv
