0db1ed44b1a7090a8d0eeb037b5037433f4003a1
Lubomir Rajter <lubomir.rajter@gmail.com>
2023-02-09 13:43:53 +0100

    per sample script added

--- scripts/bash_scripts/tax_assign_05_per_sample.sh
+++ scripts/bash_scripts/tax_assign_05_per_sample.sh
@@ -9,24 +9,19 @@ MARKER="Full18S"
 SIM="sim97"
 RAW_DATA="../../raw_data"
 PER_SAMPLE_DIR="${RAW_DATA}/per_sample_results/${MARKER}/${CELL}/${SIM}"
-OTU_CLUST_DIR="${RAW_DATA}/OTU_clust/${MARKER}/${CELL}/${SIM}"
-SAMPLES="A3 \
-         Mock \
-         NH1 \
-         NH4 \
-         Sim17 \
-         Sim22 \
-         Th16 \
-         Th38 \
-         Th40 \
-         X17007"
+FILT_OTU_DIR="${RAW_DATA}/OTU_filtered/${MARKER}/${CELL}/${SIM}"
+RAW_READS_DIR="${RAW_DATA}/PacBio/Suthaus${MARKER}/${CELL}"
+SAMPLES=$(ls ${RAW_READS_DIR}/*reads.fastq.gz | \
+          awk -F '/' '{ print $NF }' | \
+          awk -F '_' '{ print $1 }')
+
 
 
 ####################################
 ## FILTERING OTUs FOR EACH SAMPLE ##
 ####################################
 
-echo "Working with ${SIM} OTUs from ${MARKER} marker and ${CELL} cell."
+echo "Working on ${MARKER} ${CELL} ${SIM}."
 
 # Getting a fasta file with the OTU sequences for each sample
 
@@ -37,7 +32,7 @@ rm -f ${PER_SAMPLE_DIR}/transposed_table.qz*
 # Transposing the table (this will flip the sample and feature axes, necessary for the next step).
 echo "Transposing OTU table"
 qiime feature-table transpose \
-  --i-table ${OTU_CLUST_DIR}/otu_table.qza \
+  --i-table ${FILT_OTU_DIR}/otu_table_filtered.qza \
   --o-transposed-feature-table ${PER_SAMPLE_DIR}/transposed_table.qza
 
 qiime metadata tabulate \
@@ -49,29 +44,29 @@ echo "Using transposed table for filtering OTUs into individual samples"
 
 for SAMPLE in ${SAMPLES}
 do
-  rm -f ${PER_SAMPLE_DIR}/otu_seqs_${SAMPLE}.qz
-  rm -f ${PER_SAMPLE_DIR}/fasta/otu_seqs_${SAMPLE}.fasta
+  rm -f ${PER_SAMPLE_DIR}/otu_seqs_filtered_${SAMPLE}.qz
+  rm -f ${PER_SAMPLE_DIR}/fasta/otu_seqs_filtered_${SAMPLE}.fasta
 
   # filter asv sequence table
   qiime feature-table filter-seqs \
-    --i-data ${OTU_CLUST_DIR}/otu_seqs.qza \
+    --i-data ${FILT_OTU_DIR}/otu_seqs_filtered.qza \
     --m-metadata-file ${PER_SAMPLE_DIR}/transposed_table.qza \
     --p-where ${SAMPLE}_${MARKER}_${CELL} \
-    --o-filtered-data ${PER_SAMPLE_DIR}/otu_seqs_${SAMPLE}.qza
+    --o-filtered-data ${PER_SAMPLE_DIR}/otu_seqs_filtered_${SAMPLE}.qza
 
   qiime feature-table tabulate-seqs \
-    --i-data ${PER_SAMPLE_DIR}/otu_seqs_${SAMPLE}.qza \
-    --o-visualization ${PER_SAMPLE_DIR}/otu_seqs_${SAMPLE}.qzv
+    --i-data ${PER_SAMPLE_DIR}/otu_seqs_filtered_${SAMPLE}.qza \
+    --o-visualization ${PER_SAMPLE_DIR}/otu_seqs_filtered_${SAMPLE}.qzv
 
   # 9 Convert ASV sequences from qza to fasta format
   echo "Converting the ${SAMPLE} qza file to fasta file"
 
   qiime tools export \
-    --input-path ${PER_SAMPLE_DIR}/otu_seqs_${SAMPLE}.qza \
+    --input-path ${PER_SAMPLE_DIR}/otu_seqs_filtered_${SAMPLE}.qza \
     --output-path ${PER_SAMPLE_DIR}/fasta/
 
   mv ${PER_SAMPLE_DIR}/fasta/dna-sequences.fasta \
-     ${PER_SAMPLE_DIR}/fasta/otu_seqs_${SAMPLE}.fasta
+     ${PER_SAMPLE_DIR}/fasta/otu_seqs_filtered_${SAMPLE}.fasta
 done
 
 # cleaning
