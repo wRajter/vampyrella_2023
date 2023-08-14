8d19201e5e07596fa102613080630fe31e5a0a0f
Lubomir Rajter <lubomir.rajter@gmail.com>
2023-08-14 09:27:26 +0200

    RAD denoising script added

--- scripts/scripts/tax_assign_denoise_RAD.jl
+++ scripts/scripts/tax_assign_denoise_RAD.jl
@@ -1,21 +1,49 @@
 # tax_assign_denoise.jl
 
-using Pkg
+# using Pkg
 
-Pkg.add(url="https://github.com/MurrellGroup/NextGenSeqUtils.jl")
-Pkg.add(url="https://github.com/MurrellGroup/DPMeansClustering.jl")
-Pkg.add(url="https://github.com/MurrellGroup/RobustAmpliconDenoising.jl")
+# Pkg.add(url="https://github.com/MurrellGroup/NextGenSeqUtils.jl")
+# Pkg.add(url="https://github.com/MurrellGroup/DPMeansClustering.jl")
+# Pkg.add(url="https://github.com/MurrellGroup/RobustAmpliconDenoising.jl")
 
+# Load required packages
 using NextGenSeqUtils, RobustAmpliconDenoising
 
-# Accept user input for the file path
+# Define project information
+project = "Jamy_2022"
+marker = "rDNA"
+cell = "cell"
+raw_data = "../../raw_data"
+suffix = ".fastq"
 
-filt_seqs = "../../raw_data/PacBio/Suthaus_2022_Full18S/cellCombined/filtered/Mock_18S.hifi_reads.fastq"
+# Construct the raw_reads_dir path
+raw_reads_dir = joinpath(raw_data, "PacBio", "$project" * "_" * "$marker", cell, "filtered")
 
-seqs, QVs, seq_names = read_fastq(filt_seqs)
+# List files in the raw_reads_dir directory
+files = readdir(raw_reads_dir)
 
-templates, template_sizes, template_indices = denoise(seqs)
 
-save_fasta = "../../raw_data/denoise/Suthaus_2022/Full18S/cellCombined/FAD/Mock_21R_asv.fasta"
+# Extract sample names from filenames using the specified suffix
+samples = [match(r"^(.*?)\.(.*)", file).captures[1] for file in files if endswith(file, suffix)]
 
-write_fasta(save_fasta, templates, names = ["seq$(j)_$(template_sizes[j])" for j in 1:length(template_sizes)])
+
+# Loop through each sample and perform denoising
+for sample in samples
+
+    # Construct the file path for the current sample
+    filt_seqs = "$raw_reads_dir/$sample$suffix"
+
+    # Read and denoise the sequences
+    seqs, QVs, seq_names = read_fastq(filt_seqs)
+    templates, template_sizes, template_indices = denoise(seqs)
+
+     # Construct the save_fasta path for the denoised sequences
+    save_fasta = joinpath(raw_data, "denoise", project, marker, cell, "FAD", "$sample" * "_asv.fasta")
+
+    # Write denoised sequences to a fasta file
+    write_fasta(save_fasta, templates, names = ["seq$(j)_$(template_sizes[j])" for j in 1:length(template_sizes)])
+
+    # Print a message indicating completion for the current sample
+    println("Denoising for $sample done. Fasta file save to $save_fasta")
+
+end
